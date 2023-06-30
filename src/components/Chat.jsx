4a6dac690a3acea8b6e86ce8bb10fe641d860a08
Josh Mathew <jsmathew41@gmail.com>
2023-06-30 15:05:01 -0500

    working client to client comm

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,35 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
 
-const ENDPOINT = 'http://localhost:3001'
-let socket
+const ENDPOINT = "http://localhost:3001";
+let socket;
 
 function Chat() {
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState(['dummy']);
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState(["dummy"]);
+  const [socketConnected, setSocketConnected] = useState(false);
 
-    useEffect(() => {
-        socket = io(`${ENDPOINT}`);
-    // socket.on('message', (message) => {
-    //   setMessages((messages) => [...messages, message]);
-    // });
+  useEffect(() => {
+    socket = io(`${ENDPOINT}`);
+    //socket.on("connection", () => setSocketConnected(true));
+    socket.on("message", (message) => {
+      setMessages((messages) => [...messages, message]);
+    });
   }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     if (name && message) {
-      socket.emit('sendMessage', { name, message });
-      setName('');
-      setMessage('');
+      //socket.emit("join chat", "public");
+      socket.emit("sendMessage", { name, message });
+
+      setName("");
+      setMessage("");
     }
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={name} placeholder="Your name" onChange={(event) => setName(event.target.value)} />
-        <input type="text" value={message} placeholder="Your message" onChange={(event) => setMessage(event.target.value)} />
+        <input
+          type="text"
+          value={name}
+          placeholder="Your name"
+          onChange={(event) => setName(event.target.value)}
+        />
+        <input
+          type="text"
+          value={message}
+          placeholder="Your message"
+          onChange={(event) => setMessage(event.target.value)}
+        />
         <button type="submit">Send</button>
       </form>
       <ul>
