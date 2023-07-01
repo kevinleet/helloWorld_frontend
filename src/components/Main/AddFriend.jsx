0783328f7398613eb112b6fd0fea8d654eecceed
John Brooks <johnrbrooks93@gmail.com>
2023-07-01 19:45:01 -0400

    NavBar started

---
+++ src/components/Main/AddFriend.jsx
@@ -0,0 +1,5 @@
+const AddFriend = () => {
+    return <h1>Add Friend MainBar</h1>
+}
+
+export default AddFriend
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -5,9 +5,9 @@ import MainBar from "./Main/MainBar";
 const MainPage = () => {
   return (
     <div className="flex flex-auto min-w-[1200px] min-h-[700px] border border-slate-900 rounded-lg bg-slate-900">
-      <div className="flex justify-center items-center border border-red-500 w-2/12">
+      {/* <div className="flex justify-between items-center border border-red-500 w-2/12"> */}
         <NavBar />
-      </div>
+      {/* </div> */}
 
       <div className="flex justify-center items-center border border-green-500 w-4/12">
         <SideBar />
