612d55a7e874e74a9b58ee20725c9a44ca0ef34c
Josh Mathew <jsmathew41@gmail.com>
2023-07-03 16:43:25 -0500

    updated chatItem to render latestmessage

--- src/components/Side/ChatItem.jsx
+++ src/components/Side/ChatItem.jsx
@@ -3,9 +3,7 @@ const ChatItem = (props) => {
   return (
     <ul className="text-white border mt-3" id={props.users} key={props.users}>
       <h3>
-        {props.users.map((user) => (
-          <p>{user.displayname}</p>
-        ))}
+        <p>{props.latestMessage.sender.displayname}</p>
       </h3>
       <br />
       <h5>{props.latestMessage.content}</h5>
