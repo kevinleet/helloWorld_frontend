8f5e05dc7f5da3ffd7f95f4d2c5d73a980712a88
Kevin Li <kevinli617@gmail.com>
2023-07-03 17:42:34 -0400

    added new routing logic

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,52 +1,40 @@
 import NavBar from "./Nav/NavBar";
 import SideBar from "./Side/SideBar";
-import MainBar from "./Main/MainBar";
-import ChatWindow from "./Main/ChatWindow"
-import AddFriend from "./Main/AddFriend";
-import Profile from "./Main/Profile";
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "../globals";
-import { Routes, Route } from 'react-router-dom'
+import { Outlet } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export const UsersContext = React.createContext(null);
 
 const MainPage = () => {
   const [users, setUsers] = useState(null);
 
-
   useEffect(() => {
-    const getAllUsers = async () => {
-      let response = await axios.get(`${BASE_URL}/users/get/all`);
-      setUsers(response.data);
-      console.log(response.data);
-    };
-    getAllUsers();
+    try {
+      const getAllUsers = async () => {
+        let response = await axios.get(`${BASE_URL}/users/get/all`);
+        setUsers(response.data);
+        // console.log(response.data);
+      };
+      getAllUsers();
+    } catch (error) {
+      console.log(error);
+    }
   }, []);
 
   return (
     <UsersContext.Provider value={{ users, setUsers }}>
       <div className="flex flex-auto min-w-[1200px] min-h-[700px] border border-slate-900 rounded-lg bg-slate-900">
-        {/* <div className="flex justify-between items-center border border-red-500 w-2/12"> */}
-        <NavBar  />
-        {/* </div> */}
-
-        <div className="flex justify-center items-center border border-green-500 w-3/12">
-          <SideBar />
-        </div>
+        <NavBar />
+        <SideBar />
 
         <div className="flex justify-center border border-blue-500 w-8/12">
-          <MainBar />
-          <Routes>
-              {/* <Route path="*" element={<h1>404</h1>} /> */}
-              <Route exact path="/chat" element={<ChatWindow />} />
-              <Route exact path="/addfriend" element={<AddFriend />} />
-              <Route exact path="/profile" element={<Profile />} />
-              {/* <Route exact path="/home/chat/:id" element={<ChatWindow />} /> */}
-          </Routes>
+          <ChakraProvider>
+            <Outlet />
+          </ChakraProvider>
         </div>
-        
       </div>
     </UsersContext.Provider>
   );
